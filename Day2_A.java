954873282a86e3d6e8a98e7ebcc4dde02d28dc42
Priyansh Mittal <priyansh05mittal@gmail.com>
2025-01-20 22:29:00 +0530

    Add files via upload

---
+++ Day2_A.java
@@ -0,0 +1,9 @@
+public class Day2_A {
+    public static void main(String[] args) {
+        float f = 50.55f;
+        System.out.println("Float Value is: "+ f);
+
+        int i = (int) f;
+        System.out.println("Integer value is: "+ i);
+    }
+}
